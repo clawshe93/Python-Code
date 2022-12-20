18be0171baa86981c3cc226887835c8b37db8aed
clawshe93 <clawshe93@gmail.com>
2022-12-20 03:04:15 +0000

    Added code to Hello.py file

--- Hello.py
+++ Hello.py
@@ -1,7 +1,13 @@
-my_str = "1"
+my_str = "Yo " * 4 + "Come check this crazy "
 
-my_str += " testing"
-
-my_str = my_str + " testing"
+my_str = my_str + "flow!"
 
 print (my_str)
+
+print (4.5e9)
+print (2/3 + 4/3)
+
+first = "Ross"
+last = "Taylor"
+full_name = first + " " + last
+print ("Hello " + full_name + "! You just delved into python.")
