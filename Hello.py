c41b50748416ec8cc3f8e724bd8fdb1820f957c9
clawshe93 <clawshe93@gmail.com>
2022-12-23 01:40:55 +0000

    Made changes to Hello.py file

--- Hello.py
+++ Hello.py
@@ -11,3 +11,9 @@ first = "Ross"
 last = "Taylor"
 full_name = first + " " + last
 print ("Hello " + full_name + "! You just delved into python.")
+
+middle_ball = "Squirtle"
+left_ball = "Bulbasaur"
+right_ball = "Charmander"
+
+print ("You can choose " + left_ball + ", " + middle_ball + " or " + right_ball + " as your starter companion!" )
