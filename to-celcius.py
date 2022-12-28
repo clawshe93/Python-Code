628dc3c254f5dc118f53c63094c8d6ac6d7fa89c
clawshe93 <clawshe93@gmail.com>
2022-12-28 17:17:30 +0000

    changes made to to-celcius.py file

--- to-celcius.py
+++ to-celcius.py
@@ -1,6 +1,8 @@
-fahrenheit = float(input("What temperature (in Fahrenheit) would you like converted to celcius?"))
 
 #Python implementation goes here
 
-celcius = (fahrenheit  - 32) * (5/9)
-print(fahrenheit, 'in celcius is', celcius, 'degrees!')
+fahrenheit = float(input("What temperature (in Fahrenheit) would you like converted to celcius?"))
+
+celcius = (fahrenheit  - 32) * 5 / 9
+
+print(fahrenheit, 'F is', round(celcius, 2), 'C.')
