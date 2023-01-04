b87566f23bbc94a26b007df757a621c7356aa3ba
clawshe93 <clawshe93@gmail.com>
2023-01-04 19:49:36 +0000

    Added Functions section to Hello.py file

--- Hello.py
+++ Hello.py
@@ -328,5 +328,40 @@ print(uppercase_colors)
 warm_colors = [color for color in colors if color in ['red', 'orange', 'yellow']]
 print(warm_colors)
 
+#Functions
+
+def print_something(something):
+    pass
+def print_name(name):
+    print(f"Name is {name}")
+
+print (print_name('Chris'))
+
+output = print_name("Chris")
+
+def add_two(num):
+    return num + 2
+
+result = add_two(3)
+print(result)
+
+def add(num1, num2):
+    return num1 + num2
+print (add(1, 5))
+
+def contact_card(name, age, car_model):
+    return f"{name} is {age} and drives a {car_model}"
+
+print(contact_card("Chris", 29, "Toyota Camry"))
+
+print(contact_card(age = 29, car_model = "Toyota Camry", name = "Chris"))
+
+def can_drive(age, driving_age = 16):
+    return age >= driving_age
+print (can_drive(15))
+
+
+
+
 
 
