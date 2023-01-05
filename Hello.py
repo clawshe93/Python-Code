25ee011f258c7be719ac0ae6872d410b8b3a46c7
clawshe93 <clawshe93@gmail.com>
2023-01-05 17:48:03 +0000

    Added Scopes section to Hello.py file and added Scopes.py file to repo

--- Hello.py
+++ Hello.py
@@ -389,4 +389,16 @@ print(next(fib))
 print(next(fib))
 print(next(fib))
 
-prit(list(fib))
+#Scopes
+
+y = 5
+
+def set_x(y):
+    print("Inner y:", y)
+    
+    x = y
+    y = x
+    
+set_x(10)
+
+print("Outer y:", y)
