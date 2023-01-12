3021d9879c2fa33f41f5b95342e91e50831f50bc
clawshe93 <clawshe93@gmail.com>
2023-01-12 17:54:25 +0000

    Edited Boto3_Create_Multiple_EC2_Instances.py file

--- AWS Automation/Boto3_Create_Multiple_EC2_Instances.py
+++ AWS Automation/Boto3_Create_Multiple_EC2_Instances.py
@@ -1,2 +1,8 @@
 import boto3
 
+ec2_resource=boto3.resource('ec2')
+ec2_resource.create_instances(ImageId='ami-0b0dcb5067f052a63',
+    InstanceType='t2.micro',
+    SubnetId= 'subnet-092f222bb8cbd8e22',
+    MaxCount=3,
+    MinCount=3)
